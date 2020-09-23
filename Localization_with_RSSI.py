e4874084a5bb1a51986e20285fa949dcd793a1a9
bllgg <gunathilakebllg@gmail.com>
2020-09-23 11:57:00 +0530

    modified localization

--- Localization_with_RSSI.py
+++ Localization_with_RSSI.py
@@ -115,7 +115,6 @@ start_time = time.time()
 with open('Collected_Data/moving_2m.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
-        print(row[0], row[1], row[2])
         json_data = {"seq_num": int(row[0]), "dev_id": row[2], "tx_pow": -24, "RSSI": int(row[3]), "MAC": row[1], "acc_x": row[4], "acc_y": row[5], "acc_z": row[6], "gyro_x": row[7], "gyro_y": row[8], "gyro_z": row[9], "mag_x": row[10], "mag_y": row[11], "mag_z": row[12]}
         j_d = json.dumps(json_data)
         #thr = Thread(target=localization_with_rssi, args=(j_d,))
